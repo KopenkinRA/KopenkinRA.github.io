6ee9b3a426df342748d88fcdee99dc8308dbf2fb
KopenkinRA <144021026+KopenkinRA@users.noreply.github.com>
2023-10-13 14:23:12 +0300

    Update script.js

--- web5/script.js
+++ web5/script.js
@@ -1,18 +1,20 @@
-
 function sum() {
     let x=document.getElementsByName("input1")[0].value;
     let y=document.getElementsByName("select1")[0].value;
-    let sum=parseInt(x)*parseInt(y);
-    return sum;
+    return parseInt(x)*parseInt(y);
 }
 function click(event) {
     event.preventDefault();
     let x=document.getElementsByName("input1")[0].value;
     let m = x.match(/^\d+$/);
-    if (m != null) alert("Результат составит "+sum()+" руб.");
-    else alert("Вводите только цифры.");
+    if (m !== null) {
+        alert("Результат составит "+sum()+" руб.");
+    }
+    else {
+        alert("Вводите только цифры.");
+    }
 }
-window.addEventListener('DOMContentLoaded', function (event) {
+window.addEventListener("DOMContentLoaded", function (event) {
     let but = document.getElementById("button1");
     but.addEventListener("click", click);
 });
